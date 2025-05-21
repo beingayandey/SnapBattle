c2c3a86997e5616be1d14e205a77a0592596b743
Sayan Karmakar <102944275+ignsayan@users.noreply.github.com>
2025-05-21 13:42:38 +0530

    added useAxios hook

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const useAxios = () => {
+
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const prefix = 'api';
+  const axiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/${prefix}`,
+  })
+
+  axiosInstance.interceptors.request.use((config) => {
+    config.headers['Accept'] = 'application/json';
+    config.headers['Authorization'] = localStorage.getItem('token') && `Bearer ${localStorage.getItem('token')}`;
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  })
+
+  axiosInstance.interceptors.response.use((config) => {
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  })
+
+  const apiHandler = async ({ url, headers = {}, method, data = {}, params = {} }) => {
+
+    setLoading(true);
+
+    try {
+      const result = await axiosInstance({
+        url,
+        headers,
+        method,
+        data,
+        params,
+      })
+      setResponse(result.data);
+      setError(null);
+
+    } catch (error) {
+      setError(error.response ? error.response.data : error.message);
+      setResponse(null);
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { response, error, loading, apiHandler }
+}
+
+export default useAxios
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiMail, FiLock, FiEye, FiEyeOff } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import "./Login.css";
 import { useToast } from "../../components/toast/ToastNotification";
-import useAxios from "../../api/useAxios";
-import { loginUser } from "../../api/api";
+import useAxios from "../../hooks/useAxios";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [body, setBody] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
   const [theme, setTheme] = useState("light");
   const [showPassword, setShowPassword] = useState(false);
   const { showSuccess, showError } = useToast();
-  const { callApi, loading, error } = useAxios(loginUser);
+
+  const { response, error, loading, apiHandler } = useAxios();
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
@@ -22,37 +25,34 @@ const Login = () => {
 
   const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const data = await callApi(email, password); // call the loginUser API
-      if (data?.token) {
-        // Save token or user data as needed (e.g., in localStorage)
-        localStorage.setItem("token", data.token);
-        showSuccess("Logged in successfully!");
-        // Redirect or refresh app state
-      } else {
-        showError("Login failed: Invalid response");
-      }
-    } catch (err) {
-      showError(err?.response?.data?.message || "Login failed");
+  const handleSubmit = async (form) => {
+    const data = {
+      url: '/login',
+      method: 'POST',
+      data: body,
     }
-  };
+    await apiHandler(data);
+  }
+  useEffect(() => {
+    if (response && response.data) {
+      localStorage.setItem('token', response.data.token);
+      showSuccess("Login successful!");
+    }
+    if (error) showError(error.errors || "Login failed!");
+  }, [response, error])
 
   return (
     <div className="login-container">
       <div className="login-card">
         <h2 className="login-title">Log In</h2>
-        <form onSubmit={handleSubmit} className="login-form">
+        <form action={handleSubmit} className="login-form">
           <div className="input-group">
             <div className="input-wrapper">
               <FiMail className="input-icon" />
               <input
                 type="email"
                 id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => setBody({ ...body, email: e.target.value })}
                 className="input-field"
                 placeholder="Email"
                 required
@@ -66,8 +66,7 @@ const Login = () => {
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => setBody({ ...body, password: e.target.value })}
                 className="input-field"
                 placeholder="Password"
                 required
